a800819c4cfa4fa1156241326c6c3f6210ef16f5
QuenKar <47681251+QuenKar@users.noreply.github.com>
2022-12-29 10:18:02 +0800

    feat(parser):Call Expression

--- parser/parser.go
+++ parser/parser.go
@@ -40,6 +40,7 @@ var precedences = map[token.TokenType]int{
 	token.MINUS:    SUM,
 	token.SLASH:    PRODUCT,
 	token.ASTERISK: PRODUCT,
+	token.LPAREN:   CALL,
 }
 
 // we need to look at the curToken, which is the current token under
@@ -88,14 +89,25 @@ func New(l *lexer.Lexer) *Parser {
 
 	//for infix func
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
+	// "+"
 	p.registerInfix(token.PLUS, p.parseInfixExpression)
+	// "-"
 	p.registerInfix(token.MINUS, p.parseInfixExpression)
+	// "/"
 	p.registerInfix(token.SLASH, p.parseInfixExpression)
+	// "*"
 	p.registerInfix(token.ASTERISK, p.parseInfixExpression)
+	// "=="
 	p.registerInfix(token.EQ, p.parseInfixExpression)
+	// "!="
 	p.registerInfix(token.NOT_EQ, p.parseInfixExpression)
+	// "<"
 	p.registerInfix(token.LT, p.parseInfixExpression)
+	// ">"
 	p.registerInfix(token.GT, p.parseInfixExpression)
+
+	// "("
+	p.registerInfix(token.LPAREN, p.parseCallExpression)
 
 	return p
 }
@@ -452,4 +464,38 @@ func (p *Parser) parseFunctionParameters() []*ast.Identifier {
 	}
 
 	return idents
+}
+
+func (p *Parser) parseCallExpression(function ast.Expression) ast.Expression {
+	exp := &ast.CallExpression{
+		Token:    p.curToken,
+		Function: function,
+	}
+	exp.Arguments = p.parseCallArguments()
+	return exp
+}
+
+func (p *Parser) parseCallArguments() []ast.Expression {
+	args := []ast.Expression{}
+
+	if p.peekTokenIs(token.RPAREN) {
+		p.nextToken()
+		return args
+	}
+
+	p.nextToken()
+
+	args = append(args, p.parseExpression(LOWEST))
+
+	for p.peekTokenIs(token.COMMA) {
+		p.nextToken()
+		p.nextToken()
+		args = append(args, p.parseExpression(LOWEST))
+	}
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	return args
 }
