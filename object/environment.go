9b84384a8370cd1e878b3cad186fa33e44e3d4b6
QuenKar <47681251+QuenKar@users.noreply.github.com>
2023-01-02 17:34:32 +0800

    feat(eval):can call function

--- object/environment.go
+++ object/environment.go
@@ -2,15 +2,27 @@ package object
 
 type Environment struct {
 	store map[string]Object
+	outer *Environment
+}
+
+func NewEnclosedEnvironment(o *Environment) *Environment {
+	env := &Environment{
+		store: make(map[string]Object),
+		outer: o,
+	}
+	return env
 }
 
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
-	return &Environment{store: s}
+	return &Environment{store: s, outer: nil}
 }
 
 func (e *Environment) Get(key string) (Object, bool) {
 	obj, ok := e.store[key]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(key)
+	}
 	return obj, ok
 }
 
